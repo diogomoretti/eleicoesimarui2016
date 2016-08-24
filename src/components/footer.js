2f0dd2c961b761d35eda3d00823be2084b57051a
Diogo Moretti <diogo.pearljam@gmail.com>
2016-08-23 21:54:58 -0300

    Improv footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,12 +6,15 @@ const Footer = () => {
       <div className='footer-wrapper'>
         <p className='footer-message'>
           A intenção dessa página é mostrar de uma maneira simples os candidatos para o eleitor =]
-          <br />
+        </p>
+        <p className='footer-message'>
           As informações contidas nessa página foram retiradas do <a className='footer-link' href='http://divulgacandcontas.tse.jus.br' target='_blank'>site oficial do TSE</a>, seguindo as normas da <a className='footer-link' href='http://www.tse.jus.br/transparencia/acesso-a-informacao/acesso-a-informacao' target='_blank'>transparência</a>.
-          <br />
+        </p>
+        <p className='footer-message'>
           Essa é uma aplicação open-source e sob a <a className='footer-link' href='https://pt.wikipedia.org/wiki/Licen%C3%A7a_MIT' target='_blank'>licença MIT</a>. Todo o código pode ser encontrado no <a className='footer-link' href='https://github.com/diogomoretti/eleicoesimarui2016.org' target='_blank'>Github</a>.
-          <br />
-          Caso essa página esteja infringindo alguma lei ou algum terceiro esteja sendo prejudicado, por favor entre em contato para que os devidos procedimentos sejam feitos.
+        </p>
+        <p className='footer-message'>
+          Caso essa página esteja infringindo alguma lei ou algum terceiro se sinta sendo prejudicado, por favor entre em contato para que os devidos procedimentos sejam feitos.
         </p>
       </div>
     </footer>
